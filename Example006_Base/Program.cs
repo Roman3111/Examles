0861aa04775c555c4cf4572c5e237dd584bd4d98
Roman3111 <121282733+Roman3111@users.noreply.github.com>
2023-01-19 20:07:13 +0300

    добавил Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 4;
+int b = 7;
+int c = 9;
+int d = 3;
+int e = 2;
+int max = a;
+if (a  > max) max = a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+
+Console.Write("max=");
+Console.WriteLine(max);
