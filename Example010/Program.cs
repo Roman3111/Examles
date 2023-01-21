a79b6eca16a141c09866f7415ba446224db27476
Roman3111 <121282733+Roman3111@users.noreply.github.com>
2023-01-21 21:13:18 +0300

    добавил поиск наибольшего числа

---
+++ Example010/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {2, 56, 34, 9, 6, 8, 21, 54, 8 ,9};
+int n = array.Length;
+int find = 9;
+
+int index = 0;
+
+while(index < n)
+{
+   if(array[index] == find )
+   {
+    System.Console.WriteLine(index);
+    break;
+   }
+   index++;
+}
